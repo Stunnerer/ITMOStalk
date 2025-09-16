ba36528203fd3dcb351700b2b4f0a02086dbb711
Stunnerer <semgoll824@gmail.com>
2025-09-16 12:31:28 +0300

    potential anti-ban

--- itmostalk/api.py
+++ itmostalk/api.py
@@ -34,8 +34,13 @@ class API:
                 "User-Agent": self.faker.user_agent(),
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "ru-RU,ru;q=0.9",
+                "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
                 "Priority": "u=0, i",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "cross-site",
+                "Sec-GPC": "1",
+                "Upgrade-Insecure-Requests": "1"
             }
         else:
             self.headers = headers
